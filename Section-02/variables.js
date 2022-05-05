ff82315069800d150eb7b14f68d91c92a9e81fb0
Josa Vicente <jvicenpe@gmail.com>
2022-05-05 19:57:04 +0200

    v1.0

--- Section-02/variables.js
+++ Section-02/variables.js
@@ -1,3 +1,15 @@
-let a = 10;
-var b = 10;
-const c = 10;
+
+let a = 10 ,
+    b = 20, 
+    c = 'Hola', 
+    d = 'Spiderman', 
+    x = a + b;
+
+const saludo = c + ' '+ d;
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+// console.table({a,b,c,d,x})
+
+

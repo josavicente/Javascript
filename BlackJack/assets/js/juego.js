8d5c6506d500703b395349d98da71600d71c9a6d
Josa Vicente <jvicenpe@gmail.com>
2022-05-12 23:27:23 +0200

    v1.0

---
+++ BlackJack/assets/js/juego.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * 2C 2 of Clubs (Tréboles)
+ * 2D 2 of Diamonds (Diamantes)
+ * 2H 2 of Hearts (Corazones)
+ * 2S 2 of Spades (Espadas)
+ * 
+ */
+
+
+/* Creating an empty array. */
+
+let deck = [];
+/* Creating an array with the types of cards. */
+const tipos = ['C', 'D', 'H', 'S']; 
+/* Creating an array with the special cards. */
+const especiales = ['A', 'J', 'Q', 'K']; 
+
+const crearDeck = () => {
+
+    /* Creating a deck of cards. */
+    for(let i = 2; i<= 10; i++){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+    /* Creating the special cards. */
+    for(let espc of especiales){
+        for(let tipo of tipos){
+            deck.push(espc + tipo);
+        }
+    }
+    
+    deck = _.shuffle(deck)
+    console.log(deck)
+}
+
+
+
+
+crearDeck();
+
+
